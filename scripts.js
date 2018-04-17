8a3c53de2203be0e097525194a8f869a84f7726e
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-17 19:35:37 +0200

    Add new scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+getTriangleArea 
+var a = pompt('Wprowadz liczbe'),
+	h = pompt ('Wprowadz liczbe');
+
+	function getTriangleArea(a, h) {
+		if (true) { a && h > 0}
+			else {document.write('Nieprawidłowe dane!')}
+
+	}
+	return 'a*h/2';
+
+	console.log(getTriangleArea(10, 6));
+
+	var triangle1Area = getTriangleArea(10, 15);
+	var triangle2Area = getTriangleArea(9, 18);
+	var triangle3Area = getTriangleArea(10, 20);
