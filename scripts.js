96177c9ae812a508b7bffec4de0fbea9e988037b
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-21 18:54:02 +0200

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ var x = prompt('Wprowadz liczbe'),
 		if (a>0 && h>0){
 			return a*h/2;
 		} else {
-			console.log('Nieprawidłowe dane!')
+			return 'Nieprawidłowe dane!'
 		}
 	}
 	console.log(getTriangleArea(x, y));
